e3e0378a082ced999b361b7834088166b0381cb0
Gabydc <diazcortesgb@gmail.com>
2018-02-21 20:19:13 +0100

    Version Feb 21

--- pushrepo.sh
+++ pushrepo.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+git status
 git add *
-git commit -m "First version, Abstract, Introduction and Methods"
+git status
+git commit -m "Version Feb 21"
 git remote add origin https://github.com/Gabydc/JCP18.git
 git push -u origin master
